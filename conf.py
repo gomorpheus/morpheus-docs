4458e063ae35cd18baaf5519cf2de3a882f888f8
Jeff Wheeler <jwheeler@morpheusdata.com>
2020-01-24 01:40:47 -0700

    styling updates

--- conf.py
+++ conf.py
@@ -93,7 +93,7 @@ html_theme_options = {
 'logo_only': True
 }
 
-html_logo = "_static/hpesm_spec_grn_pos_rgb_500x76.png"
+html_logo = "_static/HPE_log_left_wht.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
