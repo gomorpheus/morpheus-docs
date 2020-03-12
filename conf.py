f1edef5353499fa273578f00b6c79b693f3fc35f
Jeff Wheeler <jeff@1wheeler.com>
2020-03-11 23:50:43 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = u'Gemini Docs'
+project = u'HPE GreenLake Private Cloud'
 copyright = u'2020, HPE'
 author = u'HPE'
 
@@ -204,7 +204,7 @@ def setup(app):
     app.add_stylesheet('hpe_theme.css')
 
 rst_prolog = """
-.. |morpheus| replace:: %s """ % os.environ['COMPANY'] + """
+.. |morpheus| replace:: %s """ % os.environ[COMPANY] + """
 .. |morphbranch| replace:: v4.1
 .. |morphver| replace:: v4.1.2
 .. |rmqbranch| replace:: v3.5+
