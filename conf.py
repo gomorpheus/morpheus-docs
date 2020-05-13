39ad72c0d27fafb2fc78a8202c0b56b20f53b2b5
Jeff Wheeler <jeff@1wheeler.com>
2020-05-13 13:09:06 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -31,7 +31,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['recommonmark','sphinx_markdown_tables','sphinx_tabs.tabs','sphinx_search.extension']
+extensions = ['recommonmark','sphinx_markdown_tables','sphinx_tabs.tabs','readthedocs-sphinx-search']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
