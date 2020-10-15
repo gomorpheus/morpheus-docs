cda30ef6c8bb5ea84f33888d0e0bec49883ba018
Jeff Wheeler <jeff@1wheeler.com>
2020-10-14 22:02:25 -0600

    verison bumps

--- conf.py
+++ conf.py
@@ -203,12 +203,14 @@ rst_prolog = """
 .. |morphbranch| replace:: v4.2
 .. |morphver| replace:: v4.2.4
 .. |rmqbranch| replace:: v3.5-3.8
-.. |rmqver| replace:: v3.8.5
+.. |rmqver| replace:: v3.8.9
 .. |mysqlbranch| replace:: v5.7
-.. |mysqlver| replace:: v5.7.29
+.. |mysqlver| replace:: v5.7.31
+.. |mysqlverfips| replace:: v5.7.29
 .. |esbranch| replace:: v7.x
 .. |esver| replace:: v7.8.1
-.. |tcver| replace:: v9.0.37
+.. |tcver| replace:: v9.0.39
+.. |nginxver| replace:: v1.19.3
 .. |morphdat| replace:: Morpheus Data, LLC
 """
 
