58a69f05f459bf07a40bf5ef8e7a9e4720999ce9
Jeff Wheeler <jeff@1wheeler.com>
2020-11-17 23:34:36 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -84,7 +84,7 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_use_opensearch = 'https://docs/morpheusdata.com'
+html_use_opensearch = 'https://docs.morpheusdata.com/en/latest'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
