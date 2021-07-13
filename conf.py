6fb74671f87c3230231668bbafacbc40630c4d80
Jeff Wheeler <jeff@1wheeler.com>
2021-07-13 00:35:39 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 master_doc = 'index'
 
 latex_documents = [
-    (master_doc, 'morpheus5.2.6-1.tex', u'Morpheus Documentation',
+    (master_doc, 'morpheus5.2.8-1.tex', u'Morpheus Documentation',
      u'Morpheus', 'manual'),
 ]
 
@@ -11,7 +11,7 @@ rst_prolog = """
 .. |repo_host_url| replace:: https://downloads.morpheusdata.com
 .. |morphbranch| replace:: v5.2
 .. |morphver| replace:: v5.2.8
-.. |previousMorphVer| replace:: v5.2.5
+.. |previousMorphVer| replace:: v5.2.7
 .. |minUpgradeVer| replace:: v4.2.0
 .. |rmqbranch| replace:: v3.5-3.8
 .. |rmqver| replace:: v3.8.9
