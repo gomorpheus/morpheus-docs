41f5090afc6cbfdf56067fdf4fc528e2109193cb
Alex Harker <alexnharker@gmail.com>
2021-10-15 17:52:11 -0400

    load in new variables

--- conf.py
+++ conf.py
@@ -37,7 +37,153 @@ rst_prolog = """
 
 .. |debianVersion| replace:: 9,10,11
 
-
+.. |Lib| replace:: Library
+.. |LibAut| replace:: Library > Automation
+.. |LibAutTas| replace:: Library > Automation > Tasks
+.. |LibAutWor| replace:: Library > Automation > Workflows
+.. |LibAutSca| replace:: Library > Automation > Scale Thresholds
+.. |LibAutPow| replace:: Library > Automation > Power Scheduling
+.. |LibAutExe| replace:: Library > Automation > Execute Scheduling
+.. |LibBlu| replace:: Library > Blueprints
+.. |LibBluCat| replace:: Library > Blueprints > Catalog Items
+.. |LibBluApp| replace:: Library > Blueprints > App Blueprints
+.. |LibBluIns| replace:: Library > Blueprints > Instance Types
+.. |LibBluLay| replace:: Library > Blueprints > Layouts
+.. |LibBluNod| replace:: Library > Blueprints > Node Types
+.. |LibBluClu| replace:: Library > Blueprints > Cluster Layouts
+.. |LibVir| replace:: Library > Virtual Images
+.. |LibOpt| replace:: Library > Options
+.. |LibOptInp| replace:: Library > Options > Inputs
+.. |LibOptOpt| replace:: Library > Options > Option Lists
+.. |LibTem| replace:: Library > Templates
+.. |LibTemSpe| replace:: Library > Templates > Spec Templates
+.. |LibTemFil| replace:: Library > Templates > File Templates
+.. |LibTemScr| replace:: Library > Templates > Script Templates
+.. |LibTemSec| replace:: Library > Templates > Security Packages
+.. |Pro| replace:: Provisioning
+.. |ProCat| replace:: Provisioning > Catalog
+.. |ProCatInv| replace:: Provisioning > Catalog > Inventory
+.. |ProIns| replace:: Provisioning > Instances
+.. |ProApp| replace:: Provisioning > Apps
+.. |ProJob| replace:: Provisioning > Jobs
+.. |ProJobJob| replace:: Provisioning > Jobs > Job Executions
+.. |ProJobSec| replace:: Provisioning > Jobs > Security Packages
+.. |ProCod| replace:: Provisioning > Code
+.. |ProCodRep| replace:: Provisioning > Code > Repositories
+.. |ProCodDep| replace:: Provisioning > Code > Deployments
+.. |ProCodInt| replace:: Provisioning > Code > Integrations
+.. |Ope| replace:: Operations
+.. |OpeDas| replace:: Operations > Dashboard
+.. |OpeRep| replace:: Operations > Reports
+.. |OpeAna| replace:: Operations > Analytics
+.. |OpeGui| replace:: Operations > Guidance
+.. |OpeWik| replace:: Operations > Wiki
+.. |OpeCos| replace:: Operations > Costing
+.. |OpeCosBud| replace:: Operations > Costing > Budgets
+.. |OpeCosInv| replace:: Operations > Costing > Invoices
+.. |OpeCosUsa| replace:: Operations > Costing > Usage
+.. |OpeApp| replace:: Operations > Approvals
+.. |OpeAct| replace:: Operations > Activity
+.. |OpeActAla| replace:: Operations > Activity > Alarms
+.. |OpeActHis| replace:: Operations > Activity > History
+.. |Inf| replace:: Infrastructure
+.. |InfGro| replace:: Infrastructure > Groups
+.. |InfClo| replace:: Infrastructure > Clouds
+.. |InfClu| replace:: Infrastructure > Clusters
+.. |InfCom| replace:: Infrastructure > Compute
+.. |InfComHos| replace:: Infrastructure > Compute > Hosts
+.. |InfComVir| replace:: Infrastructure > Compute > Virtual Machines
+.. |InfComCon| replace:: Infrastructure > Compute > Containers
+.. |InfComRes| replace:: Infrastructure > Compute > Resources
+.. |InfComBar| replace:: Infrastructure > Compute > Bare Metal
+.. |InfNet| replace:: Infrastructure > Network
+.. |InfNetNet| replace:: Infrastructure > Network > Networks
+.. |InfNetNet| replace:: Infrastructure > Network > Network Groups
+.. |InfNetRou| replace:: Infrastructure > Network > Routers
+.. |InfNetIP | replace:: Infrastructure > Network > IP Pools
+.. |InfNetDom| replace:: Infrastructure > Network > Domains
+.. |InfNetPro| replace:: Infrastructure > Network > Proxies
+.. |InfNetSec| replace:: Infrastructure > Network > Security Groups
+.. |InfNetInt| replace:: Infrastructure > Network > Integrations
+.. |InfLoa| replace:: Infrastructure > Load Balancers
+.. |InfLoaLoa| replace:: Infrastructure > Load Balancers > Load Balancers
+.. |InfLoaVir| replace:: Infrastructure > Load Balancers > Virtual Servers
+.. |InfSto| replace:: Infrastructure > Storage
+.. |InfStoBuc| replace:: Infrastructure > Storage > Buckers
+.. |InfStoFil| replace:: Infrastructure > Storage > File Shares
+.. |InfStoVol| replace:: Infrastructure > Storage > Volumes
+.. |InfStoDat| replace:: Infrastructure > Storage > Data Stores
+.. |InfStoSer| replace:: Infrastructure > Storage > Servers
+.. |InfKey| replace:: Infrastructure > Keys & Certs
+.. |InfKeyKey| replace:: Infrastructure > Keys & Certs > Key Pairs
+.. |InfKeySSL| replace:: Infrastructure > Keys & Certs > SSL Certificates
+.. |InfKeyInt| replace:: Infrastructure > Keys & Certs > Integrations
+.. |InfBoo| replace:: Infrastructure > Boot
+.. |InfBooMap| replace:: Infrastructure > Boot > Mapping
+.. |InfBooBoo| replace:: Infrastructure > Boot > Boot Menus
+.. |InfBooAns| replace:: Infrastructure > Boot > Answer Files
+.. |InfBooIma| replace:: Infrastructure > Boot > Images
+.. |InfBooDis| replace:: Infrastructure > Boot > Discovered MAC Addresses
+.. |InfBoo| replace:: Infrastructure > Boot
+.. |Bac| replace:: Backups
+.. |BacSum| replace:: Backups > Summary
+.. |BacJob| replace:: Backups > Jobs
+.. |BacBac| replace:: Backups > Backups
+.. |BacHis| replace:: Backups > History
+.. |BacHisBac| replace:: Backups > History > Backups
+.. |BacHisRes| replace:: Backups > History > Restores
+.. |BacInt| replace:: Backups > Integrations
+.. |Mon| replace:: Monitoring
+.. |MonSta| replace:: Monitoring > Status
+.. |MonLog| replace:: Monitoring > Logs
+.. |MonApp| replace:: Monitoring > Apps
+.. |MonChe| replace:: Monitoring > Checks
+.. |MonGro| replace:: Monitoring > Groups
+.. |MonInc| replace:: Monitoring > Incidents
+.. |MonCon| replace:: Monitoring > Contacts
+.. |MonAle| replace:: Monitoring > Alert Rules
+.. |Too| replace:: Tools
+.. |TooCyp| replace:: Tools > Cypher
+.. |TooArc| replace:: Tools > Archives
+.. |TooIma| replace:: Tools > Image Builder
+.. |TooImaIma| replace:: Tools > Image Builder > Image Builds
+.. |TooImaBoo| replace:: Tools > Image Builder > Boot Scripts
+.. |TooImaPre| replace:: Tools > Image Builder > Preseed Scripts
+.. |TooVDI| replace:: Tools > VDI Pools
+.. |TooVDIVDI| replace:: Tools > VDI Pools > VDI Pools
+.. |TooVDIVDI| replace:: Tools > VDI Pools > VDI Apps
+.. |TooVDIVDI| replace:: Tools > VDI Pools > VDI Gateways
+.. |Adm| replace:: Administration
+.. |AdmTen| replace:: Administration > Tenants
+.. |AdmPla| replace:: Administration > Plans & Pricing
+.. |AdmPlaPla| replace:: Administration > Plans & Pricing > Plans
+.. |AdmPlaPri| replace:: Administration > Plans & Pricing > Price Sets
+.. |AdmPlaPri| replace:: Administration > Plans & Pricing > Prices
+.. |AdmRol| replace:: Administration > Roles
+.. |AdmUse| replace:: Administration > Users
+.. |AdmUseUse| replace:: Administration > Users > Users
+.. |AdmUseUse| replace:: Administration > Users > User Groups
+.. |AdmInt| replace:: Administration > Integrations
+.. |AdmIntInt| replace:: Administration > Integrations > Integrations
+.. |AdmIntPac| replace:: Administration > Integrations > Packages
+.. |AdmIntPlu| replace:: Administration > Integrations > Plugins
+.. |AdmIntDis| replace:: Administration > Integrations > Distributed Workers
+.. |AdmPol| replace:: Administration > Policies
+.. |AdmHea| replace:: Administration > Health
+.. |AdmHeaMor| replace:: Administration > Health > Morpheus Health
+.. |AdmHeaMor| replace:: Administration > Health > Morpheus Logs
+.. |AdmSet| replace:: Administration > Settings
+.. |AdmSetApp| replace:: Administration > Settings > Appliance
+.. |AdmSetWhi| replace:: Administration > Settings > Whitelabel
+.. |AdmSetPro| replace:: Administration > Settings > Provisioning
+.. |AdmSetMon| replace:: Administration > Settings > Monitoring
+.. |AdmSetBac| replace:: Administration > Settings > Backups
+.. |AdmSetLog| replace:: Administration > Settings > Logs
+.. |AdmSetGui| replace:: Administration > Settings > Guidance
+.. |AdmSetEnv| replace:: Administration > Settings > Environments
+.. |AdmSetSof| replace:: Administration > Settings > Software Licenses
+.. |AdmSetLic| replace:: Administration > Settings > License
+.. |AdmSetUti| replace:: Administration > Settings > Utilities
 """
 
 
