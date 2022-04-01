48764f33f96a393c23c54620227d45d15a92fd66
Alex Harker <alexnharker@gmail.com>
2022-04-01 09:23:04 -0400

    update morphdat variable

--- conf.py
+++ conf.py
@@ -12,6 +12,7 @@ rst_prolog = """
 .. |releasedate| replace:: Mar 9 2022
 .. |morpheus| replace:: Morpheus
 .. |morphues| replace:: Morpheus
+.. |morphdat| replace:: Morpheus Data
 .. |morphbranch| replace:: v5.4
 .. |morphver| replace:: v5.4.4
 .. |minUpgradeVer| replace::
