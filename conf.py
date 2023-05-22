23349686224a58a6cf5442a26b4d8acde6593fad
Jeff Wheeler <jeff@1wheeler.com>
2023-05-22 10:45:28 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@ latex_documents = [
 
 rst_prolog = """
 
-.. |releasedate| replace:: May 17 2023
+.. |releasedate| replace:: May 22 2023
 .. |releasetype| replace:: STANDARD
 .. |morphbranch| replace:: v6.1
 .. |morphver| replace:: v6.1.1
