b6cd1cca9761ca9aa2b2aac43df1a54e05e9d42b
Alex Harker <alexnharker@gmail.com>
2023-09-12 14:42:17 -0400

    update plugin version for 6.0.7

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ rst_prolog = """
 .. |nonRollingUpgradeVer| replace:: v6.0.1
 .. |minUpgradeVer| replace:: v5.2.0
 .. |previousMorphVer| replace:: v6.0.6
-.. |pluginVer| replace:: 0.14.4
+.. |pluginVer| replace:: 0.14.5
 .. |workerVer| replace:: 5.4.8
 .. |rmqbranch| replace:: v3.5-3.12
 .. |rmqver| replace:: v3.12.2
