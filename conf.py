f6268de10f93074f9bbc60a0c079a994f9c8824b
Alex Harker <alexnharker@gmail.com>
2023-09-14 10:45:22 -0400

    update release date

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@ latex_documents = [
 
 rst_prolog = """
 
-.. |releasedate| replace:: Sep 13 2023
+.. |releasedate| replace:: Sep 14 2023
 .. |releasetype| replace:: LTS
 .. |morphbranch| replace:: v6.0
 .. |morphver| replace:: v6.0.7
