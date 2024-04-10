5658ec84748d10775f2b6a360a8591cf4d538c39
Jeff Wheeler <jeff@1wheeler.com>
2024-04-10 15:09:20 -0600

    6.2.9 release notes

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@ import datetime
 master_doc = 'index'
 
 latex_documents = [
-    (master_doc, 'morpheus_6.2.8-1.tex', u'Morpheus Documentation',
+    (master_doc, 'morpheus_6.2.9-1.tex', u'Morpheus Documentation',
      u'Morpheus', 'manual'),
 ]
 
@@ -24,20 +24,20 @@ rst_prolog = """
 .. |rmqver| replace:: v3.12.9
 .. |mysqlbranch| replace:: v5.7, v8.0
 .. |mysqlver| replace:: v8.0.36
-.. |mysqlverfips| replace:: v8.0.35
+.. |mysqlverfips| replace:: v8.0.36
 .. |esbranch| replace:: v8.9+
 .. |esver| replace:: v8.11.2
 .. |tcver| replace:: v9.0.83
 .. |nginxver| replace:: v1.25.1
-.. |nodePackageVer| replace:: 3.2.22
-.. |linuxagentver| replace:: v2.5.4
+.. |nodePackageVer| replace:: 3.2.23
+.. |linuxagentver| replace:: v2.6.0
 .. |macagentver| replace:: v2.4.0
 .. |winagentver| replace:: v2.5.0.0
 .. |java| replace:: 11.0.22
 .. |java-mac| replace:: 11.0.14+9
 .. |openjdk-jre| replace:: 11.0.20+8
 .. |openjdk-elasticsearch| replace::  17.0.5+8
-.. |openssl| replace:: 1.1.1t
+.. |openssl| replace:: 1.1.1w
 .. |openssl_fips| replace:: 1.0.2u
 .. |erlang| replace:: 26.1.2
 .. |mastertenant| replace:: Master Tenant
--- release_notes/compatibility.rst
+++ release_notes/compatibility.rst
@@ -84,11 +84,11 @@ Services
    * - MySQL
      - |mysqlbranch|
      - |mysqlver|
-     -
+     - |checkmark|
    * - MySQL (FIPS)
      - |mysqlbranch|
      - |mysqlverfips|
-     -
+     - |checkmark|
    * - Elasticsearch
      - |esbranch|
      - |esver|
@@ -100,7 +100,7 @@ Services
    * - Tomcat
      - 
      - |tcver|
-     - 
+     - |checkmark|
    * - Nginx
      -
      - |nginxver|
@@ -108,11 +108,11 @@ Services
    * - OpenSSL
      -
      - |openssl|, |openssl_fips| (FIPS)
-     -
+     - |checkmark|
    * - Java
      -
      - |java|
-     -
+     - |checkmark|
    * - Java (macOS agent)
      -
      - |java-mac|
@@ -140,7 +140,7 @@ Services
      - Morpheus Linux Agent updated to |linuxagentver| 
    * - Morpheus Windows Agent
      - |winagentver|
-     - Morpheus Windows Agent updated to |winagentver|
+     - No changes
    * - Morpheus macOS Agent
      - |macagentver|
      - No changes
