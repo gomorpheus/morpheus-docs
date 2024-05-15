a730b1c945fdb24d7ca297bed8f73e26245fff41
Jeff Wheeler <jeff@1wheeler.com>
2024-05-15 16:28:46 -0600

    update morphver

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ rst_prolog = """
 .. |releasetype| replace:: LTS
 .. |morphAnnualVer| replace:: v7
 .. |morphbranch| replace:: v7.0
-.. |morphver| replace:: v7.0.1
+.. |morphver| replace:: v7.0.2
 .. |minUpgradeVer| replace:: v6.0.0
 .. |minRollingUpgradeVer| replace:: v6.0.7
 .. |nonRollingUpgradeVer| replace:: v6.0.6
