686f40ed61b520e148a4d1f1ae60c5f8fc92272d
Jeff Wheeler <jeff@1wheeler.com>
2024-10-29 16:29:30 -0600

    Update conf.py

--- hpe/conf.py
+++ hpe/conf.py
@@ -1,11 +1,11 @@
 import datetime
 import os
-import sys
 from pathlib import Path
+import sys
+sys.path.insert(0, str(Path(__file__).parent))
 
-sys.path.insert(0, "..")
 
-master_doc = '../index'
+master_doc = 'index'
 
 latex_documents = [
     (master_doc, 'hp-vm-essentials.tex', u'HPE VM Essentials Documentation',
