44e80ff0d406d609f599a7dc6d74c0850d55e136
Jeff Wheeler <jeff@1wheeler.com>
2024-10-29 15:32:29 -0600

    try hpe/source/conf.py

--- hpe/conf.py
+++ hpe/conf.py
@@ -252,7 +252,7 @@ rst_prolog = """
 
 year = datetime.datetime.now().date().strftime("%Y")
 extensions = ['myst_parser','sphinx.ext.autosectionlabel','sphinx_rtd_theme','sphinx_tabs.tabs','sphinxcontrib.contentui','sphinxcontrib.images','sphinx_search.extension','notfound.extension','sphinx.ext.autosectionlabel'] #sphinx_tabs
-templates_path = ['_templates']
+templates_path = ['/_templates']
 default_dark_mode = False
 source_suffix = ['.rst', '.md']
 project = u'Morpheus Docs'
@@ -277,8 +277,8 @@ html_theme_options = {
 'sticky_navigation': True,
 'navigation_depth': 5,
 }
-html_logo = "_static/logo.svg"
-html_static_path = ['_static']
+html_logo = "/_static/logo.svg"
+html_static_path = ['/_static']
 
 html_sidebars = {
     '**': [
@@ -302,7 +302,7 @@ def setup(app):
 context = {
     'display_github': False,
 }
-html_favicon = "_static/morpheus_fav_64.ico"
+html_favicon = "/_static/morpheus_fav_64.ico"
 htmlhelp_basename = 'morpheusdocs'
 
 latex_elements = {
