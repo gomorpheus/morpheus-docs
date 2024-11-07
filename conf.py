a3e7ddf57647759bf159b448b20c8321643dd5b0
Alex Harker <alexnharker@gmail.com>
2024-11-07 16:35:30 -0500

    set up for 7.0.8

--- conf.py
+++ conf.py
@@ -9,16 +9,16 @@ latex_documents = [
 
 rst_prolog = """
 
-.. |releasedate| replace:: October 11 2024
+.. |releasedate| replace:: November 12 2024
 .. |releasetype| replace:: LTS
 .. |morphAnnualVer| replace:: v7
 .. |morphbranch| replace:: v7.0
-.. |morphver| replace:: v7.0.7
+.. |morphver| replace:: v7.0.8
 .. |minUpgradeVer| replace:: v6.0.0
 .. |minRollingUpgradeVer| replace:: v7.0.3,v6.2.11
 .. |nonRollingUpgradeVer| replace:: v6.2.10,v7.0.2
-.. |previousMorphVer| replace:: v7.0.6
-.. |pluginVer| replace:: 1.1.8
+.. |previousMorphVer| replace:: v7.0.7
+.. |pluginVer| replace:: 1.1.9
 .. |workerVer| replace:: 5.4.8+
 .. |rmqbranch| replace:: v3.5-3.12
 .. |rmqver| replace:: v3.12.9
