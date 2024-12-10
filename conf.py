e3ed16fd5b388264d27d6572afe1a579ce50208f
Alex Harker <alexnharker@gmail.com>
2024-12-10 15:55:03 -0500

    clear old release notes and update conf.py

--- conf.py
+++ conf.py
@@ -9,16 +9,16 @@ latex_documents = [
 
 rst_prolog = """
 
-.. |releasedate| replace:: November 15 2024
+.. |releasedate| replace:: December 10 2024
 .. |releasetype| replace:: LTS
 .. |morphAnnualVer| replace:: v8
 .. |morphbranch| replace:: v8.0
-.. |morphver| replace:: v8.0.0
+.. |morphver| replace:: v8.0.1
 .. |minUpgradeVer| replace:: v6.0.0
 .. |minRollingUpgradeVer| replace:: v7.0.3,v6.2.11
 .. |nonRollingUpgradeVer| replace:: v6.2.10,v7.0.2
-.. |previousMorphVer| replace:: v7.0.7
-.. |pluginVer| replace:: 1.2.0
+.. |previousMorphVer| replace:: v8.0.0
+.. |pluginVer| replace:: 1.2.1
 .. |workerVer| replace:: 5.4.8+
 .. |rmqbranch| replace:: v3.5-3.12
 .. |rmqver| replace:: v3.12.9
